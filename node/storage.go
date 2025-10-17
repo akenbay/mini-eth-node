3ab40f5f8cbdcbd8184c40d7034867a482c0824b
Aibar <akenbay@icloud.com>
2025-10-17 20:49:10 +0500

    Feat: saving and getting the block from the storage

--- node/storage.go
+++ node/storage.go
@@ -1,6 +1,10 @@
 package node
 
-import "github.com/syndtr/goleveldb/leveldb"
+import (
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
 
 type DB struct {
 	db *leveldb.DB
@@ -12,4 +16,20 @@ func newDatabase(path string) (*DB, error) {
 		return nil, err
 	}
 	return &DB{db: db}, nil
+}
+
+func (d *DB) SaveBlock(num uint64, hash string) error {
+	key := []byte(fmt.Sprintf("block_%d", num))
+	return d.db.Put(key, []byte(hash), nil)
+}
+
+func (d *DB) GetBlock(num uint64) (string, error) {
+	key := []byte(fmt.Sprintf("block_%d", num))
+	data, err := d.db.Get(key, nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
 }
